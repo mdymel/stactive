3c03fe4eccadf98d06a279ecb91c84572f01056b
Michal Dymel <michal@dymel.pl>
2017-03-19 17:58:20 +0100

    Middleware added

---
+++ src/Stactive/StactiveMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Stactive
+{
+    public class StactiveMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public StactiveMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            
+
+            // Call the next delegate/middleware in the pipeline
+            return _next(context);
+        }
+    }
+}
---
+++ src/Stactive/StactiveMiddlewareExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Stactive
+{
+    public static class StactiveMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestCulture(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<StactiveMiddleware>();
+        }
+    }
+}
